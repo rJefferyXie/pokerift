b644008907bd7793ff2f33daf408a5969bc238ad
Jeffery <jeffxie27@outlook.com>
2023-04-22 15:22:44 -0600

    Added nextjs image export optimizer.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: "custom",
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    nextImageExportOptimizer: {
+      imageFolderPath: "public/images",
+      exportFolderPath: "out",
+      quality: 100,
+    },
+  },
+  env: {
+    storePicturesInWEBP: true,
+    generateAndUseBlurImages: true
+  },
+  trailingSlash: true
 }
 
 module.exports = nextConfig
