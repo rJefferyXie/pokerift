ca083a1c7419fa6a6f03f4a62c38679fea86f497
Jeffery Xie <jeffxie27@outlook.com>
2023-04-28 12:59:05 -0600

    Fix next image export optimizer configuration error.

--- next.config.js
+++ next.config.js
@@ -5,10 +5,14 @@ const nextConfig = {
     loader: "custom",
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    nextImageExportOptimizer: {
-      imageFolderPath: "public/images",
-      exportFolderPath: "out",
-      quality: 100,
+    transpilePackages: ["next-image-export-optimizer"],
+    env: {
+      nextImageExportOptimizer_imageFolderPath: "public/images",
+      nextImageExportOptimizer_exportFolderPath: "out",
+      nextImageExportOptimizer_quality: 75,
+      nextImageExportOptimizer_storePicturesInWEBP: true,
+      nextImageExportOptimizer_exportFolderName: "nextImageExportOptimizer",
+      nextImageExportOptimizer_generateAndUseBlurImages: true,
     },
   },
   env: {
