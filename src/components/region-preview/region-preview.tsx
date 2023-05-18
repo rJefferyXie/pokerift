78ef7fe78f088d9adb6fce8d6352de8aa245c570
Jeffery <jeffxie27@outlook.com>
2023-05-18 15:43:32 -0600

    Created region preview.

---
+++ src/components/region-preview/region-preview.tsx
@@ -0,0 +1,51 @@
+// Styling
+import styles from './region-preview.module.scss';
+
+// Constants
+import { Regions, Region } from '../../constants/regions';
+import ExportedImage from 'next-image-export-optimizer';
+
+const RegionPreview = () => {
+  return (
+    <div className={styles.container}>
+      <h2>Choose A Region!</h2>
+
+      <div className={styles.regions}>
+        {Regions.map((region: Region, idx) => {
+          return (
+            <button className={styles.region} key={idx}>
+              <h2 className={styles.regionName}>{region.name}</h2>
+              <p>{region.description}</p>
+
+              <ExportedImage
+                className={styles.wallpaper}
+                layout="fill"
+                src={region.image}
+                alt={"An image of " + region.name}
+              >
+              </ExportedImage>
+
+              {region.highlightedPokemon.map((pokemon, idx) => {
+                return (
+                  <ExportedImage
+                    className={styles.pokemonImage}
+                    layout='fixed'
+                    key={idx}
+                    width={64}
+                    height={64}
+                    alt={pokemon}
+                    src={'images/pokemon/' + pokemon + ".png"}
+                  >
+                  </ExportedImage>
+                )
+              })}
+            </button>
+          )
+        })}
+      </div>
+
+    </div>
+  );
+}
+
+export default RegionPreview;
