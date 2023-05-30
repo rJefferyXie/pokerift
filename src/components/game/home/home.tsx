e74d5232650f9d7e9f0abdaa34aae640fba6792a
Jeffery <jeffxie27@outlook.com>
2023-05-30 13:43:30 -0600

    Home page for selecting game mode and starting the game.

--- src/components/game/home/home.tsx
+++ src/components/game/home/home.tsx
@@ -1,12 +1,50 @@
 // Styles
 import styles from './home.module.scss';
 
+// React + Next
+import ExportedImage from 'next-image-export-optimizer';
+
+// Images
+import singleplayer from '../../../../public/images/wallpapers/singleplayer.jpg';
+import multiplayer from '../../../../public/images/wallpapers/multiplayer.jpg';
+
 const Home = () => {
 
   return (
     <div className={styles.container}>
-      <button>Single Player</button>
-      <button>Multi Player (coming soon)</button>
+      <h1 className={styles.title}>Select a Game Mode!</h1>
+
+      <div className={styles.buttonContainer}>
+        <button className={styles.playButton} tabIndex={0}>
+          <h2 className={styles.buttonText}>
+            Single Player
+          </h2>
+
+          <ExportedImage
+            className={styles.buttonImage}
+            width={500}
+            height={500}
+            src={singleplayer.src}
+            alt={"An image of charizard."}
+          >
+          </ExportedImage>
+        </button>
+
+        <button className={styles.playButton} tabIndex={0}>
+          <h2 className={styles.buttonText}>
+            Multi Player (coming soon)
+          </h2>
+
+          <ExportedImage
+            className={styles.buttonImage}
+            width={500}
+            height={500}
+            src={multiplayer.src}
+            alt={"An image of charizard."}
+          >
+          </ExportedImage>
+        </button>
+      </div>
     </div>
   )
 }
