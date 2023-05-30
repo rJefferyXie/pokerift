731fdc4165f0494f513597b1c2ddc6d0a0d8ae5f
Jeffery <jeffxie27@outlook.com>
2023-05-30 13:43:51 -0600

    Script for selecting 5 random pokemon based on card draw chance.

---
+++ src/scripts/openCardPack.ts
@@ -0,0 +1,17 @@
+const chooseCardsByRarity = (deck, numCards) => {
+  const pool = deck.flatMap((card) => {
+    const weight = card.draw_chance;
+    return Array(weight).fill(card);
+  });
+
+  // Fill the chosenCards array with randomly selected cards from the pool
+  const chosenCards = [];
+  while (chosenCards.length < numCards && pool.length > 0) {
+    const randomIndex = Math.floor(Math.random() * pool.length);
+    chosenCards.push(pool.splice(randomIndex, 1)[0]);
+  }
+
+  return chosenCards;
+}
+
+export default chooseCardsByRarity;
