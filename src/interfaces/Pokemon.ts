21939b15628b59e6ff7dd71c7345043e43385c6c
Jeffery <jeffxie27@outlook.com>
2023-06-05 16:50:33 -0600

    Added level.

--- src/interfaces/Pokemon.ts
+++ src/interfaces/Pokemon.ts
@@ -1,16 +1,43 @@
 interface PokemonTypes {
+  slot: number,
   type: {
-    [key: string]: string;
+    name: string,
+    url: string
   }
 }
 
+interface PokemonStats {
+  base_stat: number,
+  effort: number,
+  stat: {
+    name: string,
+    url: string
+  }
+}
+
+interface PokemonEvolutions {
+  evolvesTo: string,
+  minLevel: number
+}
+
 interface Pokemon {
+  id: number,
   name: string,
+  genus: string,
+  height: number,
+  weight: number,
+  level?: number,
   draw_chance: number,
-  types: PokemonTypes[]
+  flavor_text: string,
+  is_baby: boolean,
+  is_mythical: boolean,
+  is_legendary: boolean,
+  types: PokemonTypes[],
+  stats: PokemonStats[],
+  evolutions: PokemonEvolutions[],
   sprites: {
     [key: string]: string
-  }
+  },
 }
 
 export default Pokemon;
