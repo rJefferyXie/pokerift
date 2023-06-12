e6e38779068373d221a055fdc44c8593dd38f289
Jeffery <jeffxie27@outlook.com>
2023-06-12 09:58:41 -0600

    Added alert reducer.

--- src/store/reducers/rootReducer.tsx
+++ src/store/reducers/rootReducer.tsx
@@ -4,10 +4,12 @@ import { combineReducers } from '@reduxjs/toolkit';
 // Reducers
 import deckReducer from './deckReducer';
 import cardReducer from './cardReducer';
+import alertReducer from './alertReducer';
 
 const rootReducer = combineReducers({
   deck: deckReducer,
-  card: cardReducer
+  card: cardReducer,
+  alert: alertReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
